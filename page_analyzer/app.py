af51ec2b09f99f557c30ca90d8f9d5383dff205d
Sergei Dmitriev <sergei.dmitriev@vk.team>
2024-02-12 15:08:38 +0300

    Step 2

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -10,9 +10,9 @@ from flask import (
 )
 from dotenv import load_dotenv
 from .validator import validate
-import os
+from urllib.parse import urlparse
+import os, sys, datetime
 import psycopg2
-import datetime
 
 load_dotenv()
 
@@ -22,41 +22,52 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 DATABASE_URL = os.getenv('DATABASE_URL')
 conn = psycopg2.connect(DATABASE_URL)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-    errors = ''
+    return render_template('index.html')
 
-    if request.method == "GET":
-        return render_template('index.html', errors=errors)
-
-    if request.method == "POST":
-        name = request.form.to_dict['name']
-        errors = validate(name)
-        if errors:
-             return render_template('index.html', errors=errors)
-        else:
-             cur = conn.cursor()
-             cur.execute("insert into urls (name, created_at) values (%(name)s, %(date)s) returning id", {'name': name, 'date': datetime.datetime.now()})
-             id = cur.fetchone()[0]
-             cur.close()
-             conn.close()
-             return redirect(url_for(site_detail, id))
-
-@app.route('/urls')
+@app.route('/urls', methods=['GET', 'POST'])
 def sites():
-        cur = conn.cursor()
-        cur.execute("SELECT id, name from urls")
-        sites = cur.fetchall()
-        cur.close()
-        conn.close()
+    if request.method == "GET":
+        with conn.cursor() as cur:
+            cur.execute("SELECT id, name from urls")
+            sites = []
+            for row in cur.fetchall():
+                site = {}
+                site['id'] = row[0]
+                site['name'] = row[1]
+                sites.append(site)
         return render_template('sites.html', sites=sites)
-
+    
+    if request.method == "POST":
+        url = request.form.to_dict()['url']
+        errors = validate(url)
+        if errors:
+            flash(errors, 'error')
+            return redirect(url_for('index'))
+        else:
+            url = urlparse(url)
+            name = url.scheme + "://" + url.netloc # normalize name
+            with conn.cursor() as cur:
+                cur.execute("select id from urls where name = %s", (name,))
+                data = cur.fetchone()
+                if not data:
+                    cur.execute("insert into urls (name, created_at) values (%(name)s, %(date)s) returning id", {'name': name, 'date': datetime.datetime.now()})
+                    id = cur.fetchone()[0]
+                    conn.commit()
+                    flash('Страница успешно добавлена')
+                else:
+                    id = data[0]
+                    flash('Страница уже существует')
+            return redirect(url_for('site_detail', id=id))
 
 @app.route('/urls/<id>')
 def site_detail(id):
-    cur = conn.cursor()
-    cur.execute("SELECT id, name, created_at from urls where id = %s", (id,))
-    site = cur.fetchone()
-    cur.close()
-    conn.close()
+    with conn.cursor() as cur:
+        cur.execute("SELECT id, name, created_at from urls where id = %s", (id,))
+        site = {}
+        data = cur.fetchone()
+        site['id'] = data[0]
+        site['name'] = data[1]
+        site['created_at'] = data[2]
     return render_template('site_detail.html', site=site)
--- page_analyzer/validator.py
+++ page_analyzer/validator.py
@@ -1,5 +1,11 @@
+import validators
+
 def validate(name):
     errors = ''
-    if not name:
+    if len(name) == 0:
         errors = "URL обязателен"
+    if len(name) > 255:
+        errors = "URL превышает 255 символов"
+    if not validators.url(name):
+        errors = "Некорректный URL"
     return errors
